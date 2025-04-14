03ab4e6f278d8eb595530c8f8f4207c326870b4f
donnyunn <donnyunn@gmail.com>
2025-04-14 11:53:00 +0900

    filtering small data for noise canceling

--- PressureMapViewer/MainWindow.xaml.cs
+++ PressureMapViewer/MainWindow.xaml.cs
@@ -107,6 +107,11 @@ namespace PressureMapViewer
                 _lastFpsCheck = DateTime.Now;
             }
 
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] < 5) data[i] = 0;
+            }
+
             Update2D(data);
             UpdateCenterOfPressure(data);
             UpdateBalanceGauges(data);
